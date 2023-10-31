174787faa74e5b6d0169fbba03b82fbb13661551
c4miloarriagada <camilo.arriagadav@gmail.com>
2023-10-30 23:17:18 -0300

    config providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Router, Route, Routes } from '@solidjs/router'
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App() {
-
   return (
-    <div class="bg-slate-900  text-white">
-      <h1>Fi</h1>
-      
+    <div class="bg-slate-900 h-screen text-white">
+    <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" component={() => <></>} />
+        </Routes>
+      </Router>
+      <Footer/>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import './index.css'
 import App from './App'
 
+const queryClient = new QueryClient()
+
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+render(
+  () => (
+    <QueryClientProvider client={queryClient}>
+        {' '}
+        <App />
+    </QueryClientProvider>
+  ),
+  root!
+)
